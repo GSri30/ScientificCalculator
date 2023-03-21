66c20601592ccec7507234a7bc0fca0e6b889246
GSri30 <sri_3007@hotmail.com>
2023-03-21 11:13:43 +0530

    add: cal-app

--- ScientificCalculator/src/main/java/org/example/App.java
+++ ScientificCalculator/src/main/java/org/example/App.java
@@ -1,13 +1,128 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class App 
 {
+    private static final Logger logger = LogManager.getLogger(App.class);
+    public static void main_menu(){
+        System.out.println("\nScientific Calculator\n");
+        System.out.println("1. Square root");
+        System.out.println("2. Factorial");
+        System.out.println("3. Logarithm (base e)");
+        System.out.println("4. Power");
+        System.out.println("0. Exit");
+        System.out.println("Please enter your option: ");
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        App calculator = new App();
+
+        Scanner sc = new Scanner(System.in);
+        sc.nextLine();
+
+        int option=-1;
+        double a, b, r;
+
+        while(option!=0){
+            App.main_menu();
+            option = sc.nextInt();
+
+            switch (option){
+                case 0:
+                    System.out.println("Thank you! See you again!");
+                    break;
+                case 1:
+                    System.out.println("Enter the number to find its sqrt : ");
+                    a = sc.nextDouble();
+                    r = calculator._sqrt(a);
+                    if(r == -1){
+                        System.out.println("Invalid Input");
+                    }else{
+                        System.out.println("Square root: Input: " + a + " Output: " + r);
+                    }
+                    break;
+                case 2:
+                    System.out.println("Enter the number to find its factorial : ");
+                    a = sc.nextDouble();
+                    r = calculator._factorial(a);
+                    if(r == -1){
+                        System.out.println("Invalid Input");
+                    }else{
+                        System.out.println("Factorial: Input: " + a + " Output: " + r);
+                    }
+                    break;
+                case 3:
+                    System.out.println("Enter the number to find its logarithm (base e) : ");
+                    a = sc.nextDouble();
+                    r = calculator._log(a);
+                    if(r == -1){
+                        System.out.println("Invalid Input");
+                    }else{
+                        System.out.println("Logarithm: Input: " + a + " Output: " + r);
+                    }
+                    break;
+                case 4:
+                    System.out.println("Enter the numbers to find a power b : ");
+                    a = sc.nextDouble();
+                    b = sc.nextDouble();
+                    r = calculator._pow(a, b);
+                    if(r == -1){
+                        System.out.println("Invalid Input");
+                    }else{
+                        System.out.println("Power: Input: " + a + ", " + b + " Output: " + r);
+                    }
+                    break;
+                default:
+                    System.out.println("Please enter a valid option to proceed. You can choose from [0-4].");
+            }
+
+            if(option == 0){
+                sc.close();
+                break;
+            }
+        }
+    }
+
+    public double _sqrt(double x) {
+        if(x < 0) {
+            return -1;
+        }
+
+        double r = Math.sqrt(x);
+
+        return r;
+    }
+
+    public  double _factorial(double x){
+        if(x < 0) {
+            return -1;
+        }
+
+        double r = 1;
+        for(int i=1;i<=x;i++){
+            r *= i;
+        }
+
+        return r;
+    }
+
+    public double _log(double x) {
+        if(x <= 0){
+            return -1;
+        }
+
+        double r = Math.log(x);
+
+        return r;
+    }
+
+    public double _pow(double a, double b) {
+        double r = Math.pow(a, b);
+
+        return r;
     }
 }
--- ScientificCalculator/src/test/java/org/example/AppTest.java
+++ ScientificCalculator/src/test/java/org/example/AppTest.java
@@ -1,38 +1,56 @@
 package org.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest {
+    App calculator;
+    private double DELTA = 1e-9;
+
+    @Before
+    public void init(){
+        calculator = new App();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void sqrtTrue(){
+        assertEquals("#1 Square Root : True", 10, calculator._sqrt(100), DELTA);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void sqrtFalse(){
+        assertNotEquals("#1 Square Root : False", 5, calculator._sqrt(100), DELTA);
+    }
+
+    @Test
+    public void factorialTrue(){
+        assertEquals("#1 Factorial : True", 6, calculator._factorial(3), DELTA);
+    }
+
+    @Test
+    public void factorialFalse(){
+        assertNotEquals("#1 Factorial : False", 24, calculator._factorial(3), DELTA);
+    }
+
+    @Test
+    public void logTrue(){
+        assertEquals("#1 Log : True", 0, calculator._log(1), DELTA);
+    }
+
+    @Test
+    public void logFalse(){
+        assertNotEquals("#1 Log : False", 1, calculator._log(1), DELTA);
+    }
+
+    @Test
+    public void powerTrue(){
+        assertEquals("#1 Power : True", 9, calculator._pow(3, 2), DELTA);
+    }
+
+    @Test
+    public void powerFalse(){
+        assertNotEquals("#1 Power : False", 8, calculator._pow(3, 2), DELTA);
     }
 }
